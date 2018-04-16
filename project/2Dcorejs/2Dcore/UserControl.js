47e3148b062b956a0e2b0ce9266412cd9ed7527c
CanftIn <wwc7033@gmail.com>
2018-04-16 16:45:08 +0800

    Site updated: 2018-04-16 16:45:06

--- project/2Dcorejs/2Dcore/UserControl.js
+++ project/2Dcorejs/2Dcore/UserControl.js
@@ -1,1 +1,124 @@
-"use strict";var gObjectNum=0;function userControl(e){var t,n;if(window.event?t=e.keyCode:e.which&&(t=e.which),48<=t&&t<=57&&t-48<gEngine.Core.mAllObjects.length&&(gObjectNum=t-48),38===t&&0<gObjectNum&&gObjectNum--,40===t&&gObjectNum<gEngine.Core.mAllObjects.length-1&&gObjectNum++,87===t&&gEngine.Core.mAllObjects[gObjectNum].move(new Vec2(0,-10)),83===t&&gEngine.Core.mAllObjects[gObjectNum].move(new Vec2(0,10)),65===t&&gEngine.Core.mAllObjects[gObjectNum].move(new Vec2(-10,0)),68===t&&gEngine.Core.mAllObjects[gObjectNum].move(new Vec2(10,0)),81===t&&gEngine.Core.mAllObjects[gObjectNum].rotate(-.1),69===t&&gEngine.Core.mAllObjects[gObjectNum].rotate(.1),73===t&&(gEngine.Core.mAllObjects[gObjectNum].mVelocity.y-=1),75===t&&(gEngine.Core.mAllObjects[gObjectNum].mVelocity.y+=1),74===t&&(gEngine.Core.mAllObjects[gObjectNum].mVelocity.x-=1),76===t&&(gEngine.Core.mAllObjects[gObjectNum].mVelocity.x+=1),85===t&&(gEngine.Core.mAllObjects[gObjectNum].mAngularVelocity-=.1),79===t&&(gEngine.Core.mAllObjects[gObjectNum].mAngularVelocity+=.1),90===t&&gEngine.Core.mAllObjects[gObjectNum].updateMass(-1),88===t&&gEngine.Core.mAllObjects[gObjectNum].updateMass(1),67===t&&(gEngine.Core.mAllObjects[gObjectNum].mFriction-=.01),86===t&&(gEngine.Core.mAllObjects[gObjectNum].mFriction+=.01),66===t&&(gEngine.Core.mAllObjects[gObjectNum].mRestitution-=.01),78===t&&(gEngine.Core.mAllObjects[gObjectNum].mRestitution+=.01),188===t&&(gEngine.Core.mMovement=!gEngine.Core.mMovement),70===t)new Rectangle(new Vec2(gEngine.Core.mAllObjects[gObjectNum].mCenter.x,gEngine.Core.mAllObjects[gObjectNum].mCenter.y),100*Math.random()+10,100*Math.random()+10);if(71===t)new Circle(new Vec2(gEngine.Core.mAllObjects[gObjectNum].mCenter.x,gEngine.Core.mAllObjects[gObjectNum].mCenter.y),60*Math.random()+20);if(72===t)for(n=0;n<gEngine.Core.mAllObjects.length;n++)0!==gEngine.Core.mAllObjects[n].mInvMass&&(gEngine.Core.mAllObjects[n].mVelocity=new Vec2(20*Math.random()-10,20*Math.random()-10));82===t&&(gEngine.Core.mAllObjects.splice(5,gEngine.Core.mAllObjects.length),gObjectNum=0)}
+/* global object: gEngine */
+
+"use strict";
+var gObjectNum = 0;
+function userControl(event) {
+    var keycode;
+    
+    // platform keycode
+    if (window.event) { // IE
+        keycode = event.keyCode;
+    } else if (event.which) { // Netscape/Firefox/Opera 
+        keycode = event.which;
+    }
+
+    // 0-9
+    if (keycode >= 48 && keycode <= 57) {
+        if (keycode - 48 < gEngine.Core.mAllObjects.length) {
+            gObjectNum = keycode - 48;
+        }
+    }
+
+    // ↑ ↓
+    if (keycode === 38) { //up arrow
+        if (gObjectNum > 0) {
+            gObjectNum--;
+        }
+    }
+    if (keycode === 40) { // down arrow
+        if (gObjectNum < gEngine.Core.mAllObjects.length - 1) {
+            gObjectNum++;
+        }
+    }
+
+    // move with WASD keys
+    if (keycode === 87) { //W
+        gEngine.Core.mAllObjects[gObjectNum].move(new Vec2(0, -10));
+    }
+    if (keycode === 83) { //S
+        gEngine.Core.mAllObjects[gObjectNum].move(new Vec2(0, +10));
+    }
+    if (keycode === 65) { //A
+        gEngine.Core.mAllObjects[gObjectNum].move(new Vec2(-10, 0));
+    }
+    if (keycode === 68) { //D
+        gEngine.Core.mAllObjects[gObjectNum].move(new Vec2(10, 0));
+    }
+
+    // Rotate with QE keys
+    if (keycode === 81) { //Q
+        gEngine.Core.mAllObjects[gObjectNum].rotate(-0.1);
+    }
+    if (keycode === 69) { //E
+        gEngine.Core.mAllObjects[gObjectNum].rotate(0.1);
+    }
+
+    if (keycode === 73) { //I
+        gEngine.Core.mAllObjects[gObjectNum].mVelocity.y -= 1;
+    }
+    if (keycode === 75) { //k
+        gEngine.Core.mAllObjects[gObjectNum].mVelocity.y += 1;
+    }
+    if (keycode === 74) { //j
+        gEngine.Core.mAllObjects[gObjectNum].mVelocity.x -= 1;
+    }
+    if (keycode === 76) { //l
+        gEngine.Core.mAllObjects[gObjectNum].mVelocity.x += 1;
+    }
+    if (keycode === 85) { //U
+        gEngine.Core.mAllObjects[gObjectNum].mAngularVelocity -= 0.1;
+    }
+    if (keycode === 79) { //O
+        gEngine.Core.mAllObjects[gObjectNum].mAngularVelocity += 0.1;
+    }
+    if (keycode === 90) { //Z
+        gEngine.Core.mAllObjects[gObjectNum].updateMass(-1);
+    }
+    if (keycode === 88) { //X
+        gEngine.Core.mAllObjects[gObjectNum].updateMass(1);
+    }
+    if (keycode === 67) { //C
+        gEngine.Core.mAllObjects[gObjectNum].mFriction -= 0.01;
+    }
+    if (keycode === 86) { //V
+        gEngine.Core.mAllObjects[gObjectNum].mFriction += 0.01;
+    }
+    if (keycode === 66) { //B
+        gEngine.Core.mAllObjects[gObjectNum].mRestitution -= 0.01;
+    }
+    if (keycode === 78) { //N
+        gEngine.Core.mAllObjects[gObjectNum].mRestitution += 0.01;
+    }
+    if (keycode === 188) { //,
+        gEngine.Core.mMovement = !gEngine.Core.mMovement;
+    }
+
+
+    // draw rectangle && circle
+    if (keycode === 70) {//f
+        var r1 = new Rectangle(new Vec2(gEngine.Core.mAllObjects[gObjectNum].mCenter.x,
+                gEngine.Core.mAllObjects[gObjectNum].mCenter.y),
+                Math.random() * 100 + 10, Math.random() * 100 + 10);
+    }
+    if (keycode === 71) {//g
+        var r1 = new Circle(new Vec2(gEngine.Core.mAllObjects[gObjectNum].mCenter.x,
+                gEngine.Core.mAllObjects[gObjectNum].mCenter.y),
+                Math.random() * 60 + 20);
+    }
+
+    if (keycode === 72) {
+        //H
+        var i;
+        for (i = 0; i < gEngine.Core.mAllObjects.length; i++) {
+            if (gEngine.Core.mAllObjects[i].mInvMass !== 0) {
+                gEngine.Core.mAllObjects[i].mVelocity = new Vec2(Math.random() * 20 - 10, Math.random() * 20 - 10);
+            }
+        }
+    }
+    
+    // clear window
+    if (keycode === 82) { //R
+        gEngine.Core.mAllObjects.splice(5, gEngine.Core.mAllObjects.length);
+        gObjectNum = 0;
+    }
+}
