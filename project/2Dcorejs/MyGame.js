ef81c51b0a74ad1a6e2249e6241ea575d505d970
CanftIn <wwc7033@gmail.com>
2018-06-02 12:48:45 +0800

    Site updated: 2018-06-02 12:48:37

--- project/2Dcorejs/MyGame.js
+++ project/2Dcorejs/MyGame.js
@@ -1,1 +1,13 @@
-"use strict";function MyGame(){var e=gEngine.Core.mWidth,n=gEngine.Core.mHeight;new Rectangle(new Vec2(e/2,n/2),3,3,0),new Rectangle(new Vec2(e/2,0),e,3,0),new Rectangle(new Vec2(e/2,n),e,3,0),new Rectangle(new Vec2(0,n/2),3,n,0),new Rectangle(new Vec2(e,n/2),3,n,0)}
+/* global object: height, width, gEngine */
+
+"use strict";
+function MyGame() {
+    var width = gEngine.Core.mWidth;
+    var height = gEngine.Core.mHeight;
+    var r1 = new Rectangle(new Vec2(width / 2, height / 2), 3, 3, 0);
+
+    var up = new Rectangle(new Vec2(width / 2, 0), width, 3, 0);
+    var down = new Rectangle(new Vec2(width / 2, height), width, 3, 0);
+    var left = new Rectangle(new Vec2(0, height / 2), 3, height, 0);
+    var right = new Rectangle(new Vec2(width, height / 2), 3, height, 0);
+}
