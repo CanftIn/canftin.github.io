52241c87f532b967c42dfab1e70e95511278787d
CanftIn <wwc7033@gmail.com>
2019-03-06 15:24:34 +0800

    Site updated: 2019-03-06 15:24:08

--- project/2Dcorejs/MyGame.js
+++
@@ -1,1 +0,0 @@
-"use strict";function MyGame(){var e=gEngine.Core.mWidth,n=gEngine.Core.mHeight;new Rectangle(new Vec2(e/2,n/2),3,3,0),new Rectangle(new Vec2(e/2,0),e,3,0),new Rectangle(new Vec2(e/2,n),e,3,0),new Rectangle(new Vec2(0,n/2),3,n,0),new Rectangle(new Vec2(e,n/2),3,n,0)}
